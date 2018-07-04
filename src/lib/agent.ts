123b50057efe6c3ed720eb21e2ebd1e8f4ed2acb
YourtionGuo <yourtion@gmail.com>
2018-07-04 10:15:22 +0800

    :recycle: refactor: update agent
    only support promise for result

--- src/lib/agent.ts
+++ src/lib/agent.ts
@@ -1,56 +1,47 @@
 /**
  * @file API Agent
- * 参考 hojs
  * @author Yourtion Guo <yourtion@gmail.com>
  */
 
-import * as assert from "assert";
+import assert from "assert";
 import { IDebugger } from "debug";
-import * as stream from "stream";
+import stream from "stream";
 import { Test } from "supertest";
-import * as util from "util";
+import util from "util";
 import { create as createDebug, test as debug } from "./debug";
-import { ICallback, IKVObject, IPromiseCallback } from "./interfaces";
-import { Schema } from "./schema";
-import * as utils from "./utils";
+import { SUPPORT_METHOD, SUPPORT_METHODS } from "./api";
+import { ISourceResult } from "./utils";
 
 const defaultFormatOutput = (data: any) => [null, data];
 
 /**
  * 返回对象结构字符串
- *
- * @param {Object} obj
- * @return {String}
  */
 function inspect(obj: any) {
-  return util.inspect(obj, {
-    depth: 5,
-    colors: true,
-  });
+  return util.inspect(obj, { depth: 5, colors: true });
 }
 
 export interface ITestAgentOption {
   parent: any;
-  sourceFile: utils.ISourceResult;
-  method: string;
+  sourceFile: ISourceResult;
+  method: SUPPORT_METHODS;
   path: string;
   agent?: Test;
   takeExample: boolean;
   agentTestName?: string;
-  headers?: IKVObject;
-  input?: IKVObject;
-  output?: IKVObject;
-  agentPath?: IKVObject;
-  agentHeader?: IKVObject;
-  agentInput: IKVObject;
-  agentOutput?: IKVObject;
+  headers?: Record<string, any>;
+  input?: Record<string, any>;
+  output?: Record<string, any>;
+  agentPath?: string;
+  agentHeader?: Record<string, any>;
+  agentInput: Record<string, any>;
+  agentOutput?: Record<string, any>;
 }
 
 /**
  * 测试代理类
  */
 export class TestAgent {
-  public static SUPPORT_METHOD = Schema.SUPPORT_METHOD;
   public options: ITestAgentOption;
   public key: string;
   public debug: IDebugger;
@@ -64,27 +55,18 @@ export class TestAgent {
    * @param {Object} sourceFile 源文件路径描述对象
    * @param {Object} parent hojs实例
    */
-  constructor(
-    method: string,
-    path: string,
-    key: string,
-    sourceFile: utils.ISourceResult,
-    parent: any
-  ) {
+  constructor(method: SUPPORT_METHODS, path: string, key: string, sourceFile: ISourceResult, parent: any) {
     assert(method && typeof method === "string", "`method` must be string");
-    assert(
-      TestAgent.SUPPORT_METHOD.indexOf(method.toLowerCase()) !== -1,
-      "`method` must be one of " + TestAgent.SUPPORT_METHOD
-    );
+    assert(SUPPORT_METHOD.indexOf(method.toLowerCase()) !== -1, "`method` must be one of " + SUPPORT_METHOD);
     assert(path && typeof path === "string", "`path` must be string");
     assert(path[0] === "/", '`path` must be start with "/"');
     this.options = {
       parent,
       sourceFile,
-      method: method.toLowerCase(),
+      method: method.toLowerCase() as SUPPORT_METHODS,
       path,
       takeExample: false,
-      agentInput: {} as IKVObject,
+      agentInput: {} as Record<string, any>,
     };
     this.key = key;
     this.debug = createDebug(`agent:${this.key}`);
@@ -93,8 +75,6 @@ export class TestAgent {
 
   /**
    * 设置`supertest.Agent`实例
-   *
-   * @param {Object} agent
    */
   public setAgent(agent: Test) {
     this.debug("setAgent");
@@ -103,8 +83,6 @@ export class TestAgent {
 
   /**
    * 初始化`supertest.Agent`实例
-   *
-   * @param {Object} app Express实例
    */
   public initAgent(app: object) {
     const request = require("supertest");
@@ -116,8 +94,6 @@ export class TestAgent {
 
   /**
    * 获取测试代理
-   *
-   * @return {Object}
    */
   public agent(): TestAgent {
     debug("agent");
@@ -126,9 +102,6 @@ export class TestAgent {
 
   /**
    * 对测试结果加入文档
-   *
-   * @param {String} name 测试名
-   * @returns {Object}
    */
   public takeExample(name: string) {
     this.debug("takeExample: %s", name);
@@ -137,14 +110,14 @@ export class TestAgent {
     return this;
   }
 
-  public headers(data: IKVObject) {
+  public headers(data: Record<string, any>) {
     this.debug("headers: %j", data);
     this.options.agentHeader = data;
     Object.keys(data).forEach(k => this.options.agent && this.options.agent.set(k, data[k]));
     return this;
   }
 
-  public query(data: IKVObject) {
+  public query(data: Record<string, any>) {
     this.debug("query: %j", data);
     Object.assign(this.options.agentInput, data);
     this.options.agent!.query(data);
@@ -153,18 +126,11 @@ export class TestAgent {
 
   /**
    * 输入参数
-   *
-   * @param {Object} data
-   * @return {Object}
    */
-  public input(data: IKVObject) {
+  public input(data: Record<string, any>) {
     this.debug("input: %j", data);
     Object.assign(this.options.agentInput, data);
-    if (
-      this.options.method === "get" ||
-      this.options.method === "head" ||
-      this.options.method === "delete"
-    ) {
+    if (this.options.method === "get" || this.options.method === "delete") {
       this.options.agent!.query(data);
     } else {
       this.options.agent!.send(data);
@@ -172,9 +138,11 @@ export class TestAgent {
     return this;
   }
 
-  public attach(data: IKVObject) {
+  /**
+   * 添加 POST 参数
+   */
+  public attach(data: Record<string, any>) {
     this.debug("attach: %j", data);
-    Object.assign(this.options.agentInput, data);
     for (const i in data) {
       // TODO: use fs.ReadStream
       if (data[i] instanceof stream.Readable) {
@@ -184,63 +152,8 @@ export class TestAgent {
         this.options.agent!.field(i, data[i]);
       }
     }
+    Object.assign(this.options.agentInput, data);
     return this;
-  }
-
-  /**
-   * 期望输出成功结果
-   *
-   * @param {Function} callback
-   * @return {Promise}
-   */
-  public success(callback?: ICallback<any>) {
-    this.debug("success");
-    const cb = (callback as IPromiseCallback<any>) || utils.createPromiseCallback();
-    this.output((err, ret) => {
-      if (err) {
-        cb(new Error(`${this.key} 期望API输出成功结果，但实际输出失败结果：${inspect(err)}`));
-      } else {
-        this.saveExample();
-        cb(null, ret);
-      }
-    });
-    return cb.promise;
-  }
-
-  /**
-   * 期望输出失败结果
-   *
-   * @param {Function} callback
-   * @return {Promise}
-   */
-  public error(callback?: ICallback<any>) {
-    this.debug("error");
-    const cb = (callback as IPromiseCallback<any>) || utils.createPromiseCallback();
-    this.output((err, ret) => {
-      if (err) {
-        this.saveExample();
-        cb(null, err);
-      } else {
-        cb(new Error(`${this.key} 期望API输出失败结果，但实际输出成功结果：${inspect(ret)}`));
-      }
-    });
-    return cb.promise;
-  }
-
-  /**
-   * 获取原始输出
-   *
-   * @param {Function} callback
-   * @return {Promise}
-   */
-  public raw(callback?: ICallback<any>) {
-    this.debug("raw");
-    const cb = (callback as IPromiseCallback<any>) || utils.createPromiseCallback();
-    this.output((err, ret) => {
-      this.saveExample();
-      cb(err, ret);
-    }, true);
-    return cb.promise;
   }
 
   private saveExample() {
@@ -258,27 +171,52 @@ export class TestAgent {
 
   /**
    * 输出结果
-   *
-   * @param {Function} callback
-   * @param {Boolean} raw 原始输出
-   * @return {Promise}
    */
-  private output(cb: IPromiseCallback<any>, raw = false): Promise<any> {
+  private output(raw = false, save = false) {
     this.options.parent.api.$schemas.get(this.key).options.tested = true;
-    this.options.agent!.end((err: Error, res: IKVObject) => {
-      this.options.agentPath = res.req.path;
+    return this.options.agent!.then(res => {
+      // TODO: hack res.req.path
+      this.options.agentPath = (res as any).req.path;
       this.options.agentOutput = res.body;
-      if (err) {
-        return cb(err);
-      }
-      const formatOutputReverse =
-        this.options.parent.api.formatOutputReverse || defaultFormatOutput;
-      if (raw) {
-        return cb(null, res);
-      }
+      if (raw) return res;
+      const formatOutputReverse = this.options.parent.api.formatOutputReverse || defaultFormatOutput;
       const [err2, ret] = formatOutputReverse(res.body);
-      cb(err2, ret);
+      if (err2) throw err2;
+      if (save) this.saveExample();
+      return ret;
     });
-    return cb.promise as Promise<any>;
+  }
+
+  /**
+   * 期望输出成功结果
+   */
+  public success() {
+    this.debug("success");
+    return this.output(false, true).catch(err => {
+      throw new Error(`${this.key} 期望API输出成功结果，但实际输出失败结果：${inspect(err)}`);
+    });
+  }
+
+  /**
+   * 期望输出失败结果
+   */
+  public error() {
+    this.debug("error");
+    return this.output(false, false)
+      .then(ret => {
+        throw new Error(`${this.key} 期望API输出失败结果，但实际输出成功结果：${inspect(ret)}`);
+      })
+      .catch(err => {
+        this.saveExample();
+        return err;
+      });
+  }
+
+  /**
+   * 获取原始输出
+   */
+  public raw() {
+    this.debug("raw");
+    return this.output(true, true);
   }
 }
