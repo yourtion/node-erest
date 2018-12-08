396a2a486d3fc3874b2040d2e4c70e9e5acada3b
YourtionGuo <yourtion@gmail.com>
2018-12-08 18:24:37 +0800

    :recycle: refactor: test-params

---
+++ src/test/test-api.ts
@@ -0,0 +1,41 @@
+import os from "os";
+import { apiDelete, build, nameParams, TYPES } from "../test/helper";
+import lib from "../test/lib";
+
+const apiService = lib();
+const api = apiService.api;
+const deleteApi = apiDelete(api);
+apiService.genDocs(os.tmpdir());
+
+test("API - init", () => {
+  const apiInfo = api.$apis.get("DELETE_/index/:name")!;
+  expect(apiInfo.key).toBe("DELETE_/index/:name");
+  expect(apiInfo.options.method).toBe("delete");
+  expect(apiInfo.options.path).toBe("/index/:name");
+  expect(apiInfo.options.title).toBe("Delete");
+  expect(apiInfo.options.group).toBe("Index");
+  expect(apiInfo.options.params.name).toEqual(nameParams);
+  expect(apiInfo.options._allParams.get("name")).toEqual(nameParams);
+  expect(apiInfo.options.handler!.name).toBe("del");
+});
+
+test("API - modify", () => {
+  deleteApi.title("newTitle");
+  deleteApi.description("Yourtion");
+  const example = {
+    input: { a: "b" },
+    output: { name: "d" },
+  };
+  const outSchema = { name: nameParams };
+  deleteApi.example(example);
+  deleteApi.response(outSchema);
+  deleteApi.query({
+    numP2: build(TYPES.Number, "Number", true, 10, { max: 10, min: 0 }),
+  });
+  const apiInfo = api.$apis.get("DELETE_/index/:name")!;
+  expect(apiInfo.options.title).toBe("newTitle");
+  expect(apiInfo.options.description).toBe("Yourtion");
+  expect(apiInfo.options.examples.length).toBe(1);
+  expect(apiInfo.options.examples[0]).toEqual(example);
+  expect(apiInfo.options.response).toEqual(outSchema);
+});
--- src/test/test-params.ts
+++ src/test/test-params.ts
@@ -1,26 +1,26 @@
-import lib from "./lib";
+import lib from "../test/lib";
 
 const apiService = lib();
 
-import { build, TYPES } from "./helper";
+import { build, TYPES } from "../test/helper";
 const paramsChecker = apiService.paramsChecker();
 const schemaChecker = apiService.schemaChecker();
 
-const stringP1 = build(TYPES.String, "StringSchema", true);
-const stringP2 = build(TYPES.String, "StringSchema", true, "Hello");
-const stringP3 = build(TYPES.String, "StringSchema");
+const stringP1 = build(TYPES.String, "String1", true);
+const stringP2 = build(TYPES.String, "String2", true, "Hello");
+const stringP3 = build(TYPES.String, "String3");
 
 const numP = build(TYPES.Number, "Number", true);
-const intP = build(TYPES.Integer, "Int");
-const enumP = build(TYPES.ENUM, "Int", true, undefined, ["A", "B", 1]);
-const jsonP = build(TYPES.JSON, "Json");
+const intP = build(TYPES.Integer, "Integer");
+const enumP = build(TYPES.ENUM, "ENUM", true, undefined, ["A", "B", 1]);
+const jsonP = build(TYPES.JSON, "JSON");
 
 const schema1: Record<string, any> = { stringP2, stringP3, numP, intP };
 const array1 = build(TYPES.Array, "Array with String param", true, undefined, TYPES.Integer);
 const array2 = build(TYPES.Array, "Array with Type param", true, undefined, jsonP);
 
-describe("Params - params checker", () => {
-  it("ParamsChecker - simple checker success", () => {
+describe("ParamsChecker", () => {
+  test("simple checker success", () => {
     expect(paramsChecker("st1", "1", stringP1)).toBe("1");
     stringP3.format = true;
     expect(paramsChecker("st2", "1", stringP3)).toBe("1");
@@ -31,19 +31,19 @@ describe("Params - params checker", () => {
     expect(paramsChecker("json", '{ "a": 1 }', jsonP)).toEqual('{ "a": 1 }');
   });
 
-  it("ParamsChecker - ENUM", () => {
+  test("ENUM", () => {
     expect(paramsChecker("en1", 1, enumP)).toBe(1);
     const fn = () => paramsChecker("en2", "C", enumP);
     expect(fn).toThrow("incorrect parameter 'en2' should be valid ENUM with additional restrictions: A,B,1");
   });
 
-  it("ParamsChecker - Array with String param", () => {
+  test("Array with String param", () => {
     expect(paramsChecker("array1", ["1", 2, "99"], array1)).toEqual([1, 2, 99]);
     const fn = () => paramsChecker("array1", ["1", 2, "a"], array1);
     expect(fn).toThrow("incorrect parameter 'array1[2]' should be valid Integer");
   });
 
-  it("ParamsChecker - Array with Type param", () => {
+  test("Array with Type param", () => {
     jsonP.format = true;
     expect(paramsChecker("array2", ['{ "a": 1 }', '{ "b": 2 }', "{}"], array2)).toEqual([{ a: 1 }, { b: 2 }, {}]);
     const fn = () => paramsChecker("array2", ['{ "a": 1 }', "{"], array2);
@@ -51,33 +51,33 @@ describe("Params - params checker", () => {
   });
 });
 
-describe("Params - schema checker", () => {
-  it("SchemaChecker - success", () => {
+describe("SchemaChecker", () => {
+  test("success", () => {
     const data = { stringP2: "a", numP: 1.02, intP: 2 };
     const res = schemaChecker(data, schema1);
     expect(res).toEqual(data);
   });
 
-  it("SchemaChecker - remove not in schema success", () => {
+  test("remove not in schema success", () => {
     const data = { numP: 1.02, a: "xxx" };
     const res = schemaChecker(data, schema1);
     expect(res.a).toBeUndefined();
   });
 
-  it("SchemaChecker - requied check throw", () => {
+  test("requied check throw", () => {
     const data = { a: "xxx" };
     const fn = () => schemaChecker(data, schema1);
     expect(fn).toThrow("missing required parameter 'numP'");
   });
 
-  it("SchemaChecker - requiedOneOf check ok", () => {
+  test("requiedOneOf check ok", () => {
     const data = { numP: 123 } as any;
     const res = schemaChecker(data, schema1, ["numP", "stringP3"]);
     data.stringP2 = "Hello";
     expect(res).toEqual(data);
   });
 
-  it("SchemaChecker - requiedOneOf check throw", () => {
+  test("requiedOneOf check throw", () => {
     const data = { numP: 122 };
     const fn = () => schemaChecker(data, schema1, ["intP", "stringP3"]);
     expect(fn).toThrow("missing required parameter one of intP, stringP3 is required");
