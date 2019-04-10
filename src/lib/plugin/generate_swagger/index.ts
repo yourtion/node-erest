25f3d66b0be4299ed9adc70306059e9556a99a4c
YourtionGuo <yourtion@gmail.com>
2019-04-10 15:48:33 +0800

    :sparkles: feat: add swagger Model support

--- src/lib/plugin/generate_swagger/index.ts
+++ src/lib/plugin/generate_swagger/index.ts
@@ -9,15 +9,23 @@ import { plugin as debug } from "../../debug";
 import { IDocData, IDocWritter } from "../../extend/docs";
 import { IDocOptions } from "../..";
 import * as utils from "../../utils";
+import { ISchemaTypeFields } from "@tuzhanai/schema-manager";
+
+type SCHEMA = "http" | "https" | "ws" | "wss";
 
 interface ISwaggerResult {
+  // 指定swagger spec版本，2.0
   swagger: string;
+  // 提供API的元数据
   info: any;
-  host: string;
+  // 主机，如果没有提供，则使用文档所在的host
+  host?: string;
+  // 相对于host的路径
   basePath: string;
-  schemes: string[];
-  tags: any[];
-  definitions: any;
+  schemes: SCHEMA[];
+  // 补充的元数据，在swagger ui中，用于作为api的分组标签
+  tags: Array<{ name: string; description: string }>;
+  definitions: Record<string, ISwaggerModels>;
   paths: any;
 }
 
@@ -31,6 +39,12 @@ interface ISwaggerResultParams {
   enum?: string[];
   items?: any;
   format?: string;
+  $ref?: string;
+}
+
+interface ISwaggerModels {
+  type: string;
+  properties: Record<string, { type: string; format?: string; description: string }>;
 }
 
 export function buildSwagger(data: IDocData) {
@@ -43,9 +57,9 @@ export function buildSwagger(data: IDocData) {
       description: data.info.description,
       version: data.info.version || "1.0.0",
     },
-    host: url.host,
+    // host: url.host,
     basePath: url.pathname,
-    schemes: [url.protocol.replace(":", "")],
+    schemes: [url.protocol.replace(":", "") as SCHEMA],
     tags: [],
     definitions: {},
     paths: {},
@@ -56,18 +70,33 @@ export function buildSwagger(data: IDocData) {
   }
   result.tags.sort((a, b) => (a.name > b.name ? 1 : -1));
 
+  data.schema.forEach((value, key) => {
+    const schema = {
+      type: "object",
+      properties: {},
+    } as ISwaggerModels;
+    const fields = (value as any).fields || ({} as ISchemaTypeFields);
+    for (const item of Object.keys(fields)) {
+      schema.properties[item] = {
+        type: "string",
+        description: fields[item].comment || "",
+      };
+    }
+    result.definitions[key] = schema;
+  });
+
   const paths = result.paths;
-  const schemas = data.apis;
-  for (const [key, schema] of Object.entries(schemas)) {
+  const apis = data.apis;
+  for (const [key, api] of Object.entries(apis)) {
     const newPath = utils.getRealPath(key).replace(/:(\w+)/, "{$1}");
     if (!paths[newPath]) {
       paths[newPath] = {};
     }
     const sc = paths[newPath];
-    sc[schema.method] = {
-      tags: [schema.group],
-      summary: schema.title,
-      description: schema.description || "",
+    sc[api.method] = {
+      tags: [api.group],
+      summary: api.title,
+      description: api.description || "",
       consumes: ["application/json"],
       produces: ["application/json"],
       responses: {
@@ -77,11 +106,11 @@ export function buildSwagger(data: IDocData) {
       },
     };
 
-    sc[schema.method].parameters = [];
+    sc[api.method].parameters = [];
     const bodySchema: Record<string, any> = {};
-    let example = schema.examples && schema.examples[0];
-    if (schema.examples && schema.examples.length > 1) {
-      for (const item of schema.examples) {
+    let example = api.examples && api.examples[0];
+    if (api.examples && api.examples.length > 1) {
+      for (const item of api.examples) {
         if (item.output!.success) {
           example = item;
           break;
@@ -90,49 +119,53 @@ export function buildSwagger(data: IDocData) {
     }
     example = example || { input: {}, output: {} };
     for (const place of ["params", "query", "body"]) {
-      for (const sKey in schema[place]) {
+      for (const sKey in api[place]) {
         const obj: ISwaggerResultParams = {
           name: sKey,
           in: place === "params" ? "path" : place,
-          description: schema[place][sKey].comment,
+          description: api[place][sKey].comment,
           required: sKey === "body" ? [] : false,
-          type: "string", //schema[place][sKey].type.toLowerCase(),
+          type: api[place][sKey].type.toLowerCase(),
           example: place === "body" ? example.input![sKey] : undefined,
         };
         if (place === "params") obj.required = true;
-        if (schema.required.has(sKey)) {
+        if (api.required.has(sKey)) {
           if (place === "query") obj.required = true;
         }
-        if (schema[place][sKey].type === "ENUM") {
+        if (api[place][sKey].type === "ENUM") {
           obj.type = "string";
-          obj.enum = schema[place][sKey].params;
+          obj.enum = api[place][sKey].params;
         }
-        if (schema[place][sKey].type === "IntArray") {
+        if (api[place][sKey].type === "IntArray") {
           obj.type = "array";
           obj.items = { type: "integer" };
         }
-        if (schema[place][sKey].type === "Date") {
+        if (api[place][sKey].type === "Date") {
           obj.type = "string";
           obj.format = "date";
+        }
+        if (data.schema.has(api[place][sKey].type)) {
+          delete obj.type;
+          obj.$ref = "#/definitions/" + api[place][sKey].type;
         }
         if (place === "body") {
           delete obj.in;
           delete obj.name;
           delete obj.required;
           bodySchema[sKey] = obj;
-          if (schema.required.has(sKey) && Array.isArray(bodySchema.required)) {
+          if (api.required.has(sKey) && Array.isArray(bodySchema.required)) {
             bodySchema.required.push(sKey);
           }
         } else {
-          sc[schema.method].parameters.push(obj);
+          sc[api.method].parameters.push(obj);
         }
       }
     }
 
     // sc[schema.method].responses[200].example = example.output;
-    if (schema.method === "post" && schema.body) {
-      const required = schema.required && [...schema.required].filter(it => Object.keys(bodySchema).indexOf(it) > -1);
-      sc[schema.method].parameters.push({
+    if (api.method === "post" && api.body) {
+      const required = api.required && [...api.required].filter(it => Object.keys(bodySchema).indexOf(it) > -1);
+      sc[api.method].parameters.push({
         in: "body",
         name: "body",
         description: "请求体",
